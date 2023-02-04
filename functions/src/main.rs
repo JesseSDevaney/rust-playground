2d1cf18d0761ab9dc2191d8da17d94fa25ad55bc
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-02-04 16:31:42 -0500

    functions with return values

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,22 @@
 fn main() {
     print_labeled_measurement(5, 'h');
+
+    let x = five();
+    println!("The value of x is: {x}");
+
+    let y = plus_one(5);
+    println!("The value of y is: {y}");
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+    // x + 1; // error if semi-colon at the end
 }
