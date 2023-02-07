7910b0515395970a5d5711e80266acd4c5e518b1
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-02-07 16:56:33 -0500

    reference scope is from creation to last use
    - Note that a reference’s scope starts from where it is introduced and continues through the last time that reference is used. For instance, this code will compile because the last usage of the immutable references, the println!, occurs before the mutable reference is introduced:

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
     let mut s = String::from("hello");
 
-    let r1 = &mut s;
-    let r2 = &mut s;
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
 
-    println!("{}, {}", r1, r2);
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
