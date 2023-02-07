a268a37ce432fbc12d43e03f3f9b304c8067804f
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-02-07 17:02:09 -0500

    fix dangling reference

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let reference_to_nothing = dangle();
+    let string = no_dangle();
 }
 
-fn dangle() -> &String {
+fn no_dangle() -> String {
     let s = String::from("hello");
 
-    &s
+    s
 }
