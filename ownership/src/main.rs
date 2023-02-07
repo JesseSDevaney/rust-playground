bdecef26f10e08ba2e10dfc7ba0b9add708eadc2
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-02-07 16:08:36 -0500

    data on the heap is `moved`, not copied
    - s1 was `moved` into s2
    - Rust does this to prevent both references from freeing memory which leads to a double-free memory bug

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
-    let s1 = "hello"; // immutable string literal stored on the stack
+    let s1 = String::from("hello");
+    let s2 = s1;
 
-    let mut s = String::from("hello"); // mutable string stored on the heap (the heap is what allows it to be mutable)
-    s.push_str(", world!"); // push_str() appends a literal to a String
-    println!("{}", s);
+    println!("{}, world!", s1);
 }
