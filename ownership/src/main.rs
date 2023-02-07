1db724e3e73ad9634491f77de5e99b4b1cb4d110
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-02-07 16:24:43 -0500

    ownership with functions

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,27 @@
 fn main() {
-    let x = 5;
-    let y = x;
+    let s = String::from("hello"); // s comes into scope
 
-    println!("x = {}, y = {}", x, y);
-}
+    takes_ownership(s); // s's value moves into the function...
+                        // ... and so is no longer valid here
+
+    let x = 5; // x comes into scope
+
+    makes_copy(x); // x would move into the function,
+                   // but i32 is Copy, so it's okay to still
+                   // use x afterward
+
+    // println!("{}", s); // ! compiler error for using a reference that was moved (into takes_ownership(s))
+    // println!("{}", x); // no error because integer types implement Copy trait
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing
+  // special happens.
+
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
