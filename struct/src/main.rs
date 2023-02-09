2dd70742a8a51ea16ca147628bc40dc8247ec7a2
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-02-09 01:41:20 -0500

    creating a tuple struct

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -1,25 +1,9 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 fn main() {
-    // --snip--
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 
-    let user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
-    };
-
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
-
-    // println!("{}", user1.username); // ! errors b/c it was moved to user2.username and it is heap-only data (no Copy trait)
-    println!("{}", user1.email); // ! does not error bc it was not moved
+    println!("{} {} {}", black.0, black.1, black.2);
 }
