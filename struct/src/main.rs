2c82034324c9054b58fc32ec7ec30394cbd3ed22
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-02-09 01:30:20 -0500

    factory function to return an instance

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -5,13 +5,18 @@ struct User {
     sign_in_count: u64,
 }
 
-fn main() {
-    let mut user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
+fn build_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
         active: true,
         sign_in_count: 1,
-    };
+    }
+}
 
-    user1.email = String::from("anotheremail@example.com");
+fn main() {
+    let user1 = build_user(
+        String::from("someone@example.com"),
+        String::from("someusername123"),
+    );
 }
