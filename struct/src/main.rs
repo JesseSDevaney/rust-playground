b1131c27d24e5d4de663b09688ffc027b6d238c7
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-02-09 01:47:09 -0500

    unit-like structs

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -1,9 +1,5 @@
-struct Color(i32, i32, i32);
-struct Point(i32, i32, i32);
+struct AlwaysEqual;
 
 fn main() {
-    let black = Color(0, 0, 0);
-    let origin = Point(0, 0, 0);
-
-    println!("{} {} {}", black.0, black.1, black.2);
+    let subject = AlwaysEqual;
 }
